9fd60c432535297cc0c5d454f504b68cf7128077
christine.huber <christine.huber@AW5CG8171154>
2018-10-18 16:51:55 +0200

    Remove first index file and remove content from cli generated components

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Soon to be a drag and drop app made in Vue"/>
   </div>
 </template>
 
